0a980be49572afdf2785f19ff1d375cfea33e2b1
Joydeep Kundu <96250529+Joydeep-Kundu@users.noreply.github.com>
2023-04-12 14:04:15 +0530

    Update fibalgo.py

--- fibalgo.py
+++ fibalgo.py
@@ -1,15 +1,10 @@
-def fib(n,l=[0,1]):
-    if n==1:
-        return l[1]
-    return fib(n-1,[l[1],l[0]+l[1]])
+def fibonacii(num,lis):
+    if num==1:
+        return lis[1]
+    return fibonacii(num-1,[lis[1],lis[0]+lis[1]])
 
-def tryb(n,l=[0,0,1]):
-    if n==1:
-        return l[2]
-    return tryb(n-1,[l[1],l[2],l[1]+l[2]+l[0]])
 def main():
-    print(fib(8))
-    print(tryb(7))
+    print(fib(8,[0,1]))
 
 if __name__=='__main__':
     main()
